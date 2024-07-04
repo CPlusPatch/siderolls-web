f15746ba52ee0a7e4e0cefb4f4492f7268a347e2
Jesse Wierzbinski <contact@cpluspatch.com>
2024-07-03 14:11:12 -1000

    fix: :bug: Prevent adding links with bookmarklet several times

--- components/events/CreateParamHandler.tsx
+++ components/events/CreateParamHandler.tsx
@@ -1,35 +1,11 @@
-import { aggregator } from "@/classes/aggregator/content-aggregator";
-import { UserLinkDriver } from "@/classes/aggregator/drivers/user-link";
-import { useSearchParams } from "next/navigation";
-import { type FC, useEffect, useState } from "react";
-import { FullPageLoader } from "../loaders/full-page";
+import { FullPageLoader } from "@/components/loaders/full-page";
+import { useCreateParam } from "@/hooks/useCreateParam";
+import type { FC } from "react";
 
-export const CreateParamHandler: FC<{
-    sidepageId: string;
-}> = ({ sidepageId }) => {
-    const searchParams = useSearchParams();
-    const [loading, setLoading] = useState(false);
+export const CreateParamHandler: FC<{ sidepageId: string }> = ({
+    sidepageId,
+}) => {
+    const { loading } = useCreateParam(sidepageId);
 
-    useEffect(() => {
-        if (searchParams.get("create")) {
-            // Check if it's a link
-            if (URL.canParse(searchParams.get("create") ?? "")) {
-                setLoading(true);
-                const linkDriver = new UserLinkDriver();
-                linkDriver
-                    .parseContent({
-                        title: "",
-                        url: searchParams.get("create") ?? "",
-                    })
-                    .then((content) =>
-                        aggregator.addContentToSidepage(sidepageId, content),
-                    )
-                    .then(() => {
-                        setLoading(false);
-                    });
-            }
-        }
-    }, [searchParams, sidepageId]);
-
-    return <>{loading && <FullPageLoader text="Adding link" />}</>;
+    return loading ? <FullPageLoader text="Adding link" /> : null;
 };
