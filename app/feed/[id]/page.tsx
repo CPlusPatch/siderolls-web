9c470141c2c393551618b9c9628917efc068d5eb
Jesse Wierzbinski <contact@cpluspatch.com>
2024-08-19 17:32:35 +0200

    fix: :bug: Correct layout that was causing items to jump around

--- app/feed/[id]/page.tsx
+++ app/feed/[id]/page.tsx
@@ -1,9 +1,15 @@
 "use client";
+import TreeComponent, { type Items } from "@/components/tree/Tree";
+import {
+    addItem,
+    initializeDataProvider,
+} from "@/components/tree/dataProvider";
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useApi } from "@/lib/api";
+import { Plus } from "lucide-react";
 import type { FC } from "react";
-import TreeMain from "../trees/page";
 
 const FeedMain: FC<{
     params: {
@@ -17,9 +23,45 @@ const FeedMain: FC<{
         return null;
     }
 
+    const items: Items = {
+        root: {
+            index: "root",
+            isFolder: true,
+            children: ["child1", "child2"],
+            data: { title: "Root item" },
+        },
+        child1: {
+            index: "child1",
+            children: [],
+            data: { title: "Child item 1", url: "https://example.com" },
+        },
+        child2: {
+            index: "child2",
+            isFolder: true,
+            children: ["child3"],
+            data: { title: "Child item 2" },
+        },
+        child3: {
+            index: "child3",
+            children: [],
+            data: { title: "Child item 3" },
+        },
+    };
+
+    const dataProvider = initializeDataProvider(items);
+
+    const handleAddItem = () => {
+        const name = prompt("New item name");
+        if (!name) {
+            return;
+        }
+
+        addItem(dataProvider, items, name);
+    };
+
     return (
-        <div className="max-w-5xl mx-auto w-full p-4">
-            <Tabs defaultValue="source" className="flex flex-col grow">
+        <div className="max-w-5xl mx-auto w-full h-full p-4">
+            <Tabs defaultValue="source" className="flex flex-col grow h-full">
                 <div className="flex items-center">
                     <TabsList>
                         <TabsTrigger value="source">Source</TabsTrigger>
@@ -28,15 +70,25 @@ const FeedMain: FC<{
                 </div>
                 <TabsContent value="source" className="py-4 h-full">
                     <div className="gap-8 grid grid-cols-1 lg:grid-cols-2 w-full h-full">
-                        <div className="h-full overflow-auto py-4 ring-1 w-full ring-ring/10 rounded bg-card">
-                            <TreeMain />
-                        </div>
-                        <div className="overflow-hidden ring-1 ring-ring/10 rounded lg:max-w-xl w-full flex items-center justify-center">
-                            <img
-                                src={data.banner_image}
-                                alt="Banner"
-                                className="w-full h-full object-contain"
+                        <div className="h-fit overflow-auto p-4 ring-1 w-full ring-ring/10 rounded bg-card flex flex-col gap-4">
+                            <div className="flex flex-row gap-2">
+                                <Button onClick={handleAddItem} size={"icon"}>
+                                    <Plus className="size-4" />
+                                </Button>
+                            </div>
+                            <TreeComponent
+                                items={items}
+                                provider={dataProvider}
                             />
+                        </div>
+                        <div className="lg:max-w-xl w-full flex items-start justify-center h-fit">
+                            <div className="ring-1 ring-ring/10 flex items-center justify-center rounded overflow-hidden">
+                                <img
+                                    src={data.banner_image}
+                                    alt="Banner"
+                                    className="w-full h-full object-contain"
+                                />
+                            </div>
                         </div>
                     </div>
                 </TabsContent>
--- app/feed/trees/page.tsx
+++ app/feed/trees/page.tsx
@@ -1,58 +1,8 @@
-import TreeComponent, { type Items } from "@/components/tree/Tree";
-import {
-    addItem,
-    initializeDataProvider,
-} from "@/components/tree/dataProvider";
-import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
 import type { FC } from "react";
 
 const TreeMain: FC = () => {
-    const items: Items = {
-        root: {
-            index: "root",
-            isFolder: true,
-            children: ["child1", "child2"],
-            data: { title: "Root item" },
-        },
-        child1: {
-            index: "child1",
-            children: [],
-            data: { title: "Child item 1", url: "https://example.com" },
-        },
-        child2: {
-            index: "child2",
-            isFolder: true,
-            children: ["child3"],
-            data: { title: "Child item 2" },
-        },
-        child3: {
-            index: "child3",
-            children: [],
-            data: { title: "Child item 3" },
-        },
-    };
-
-    const dataProvider = initializeDataProvider(items);
-
-    const handleAddItem = () => {
-        const name = prompt("New item name");
-        if (!name) {
-            return;
-        }
-
-        addItem(dataProvider, items, name);
-    };
-
     return (
-        <div className="px-4 w-full h-full flex flex-col gap-4 overflow-hidden">
-            <div className="flex flex-row gap-2">
-                <Button onClick={handleAddItem} size={"icon"}>
-                    <Plus className="size-4" />
-                </Button>
-            </div>
-            <TreeComponent items={items} provider={dataProvider} />
-        </div>
+        <div className="px-4 w-full h-full flex flex-col gap-4 overflow-hidden" />
     );
 };
 
